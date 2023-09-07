95bd65a7dc5b06021c238920959a7599a9884dc6
TJ-Mindbowser <tanishq.joshi@mindbowser.com>
2023-09-07 20:32:36 +0530

    resovled cors issues

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 exports.config = {
-    PORT : 3000,
+    PORT : 8080,
     MONGO_URI : 'mongodb+srv://tutorial-user:VfBq7yaT0tBkdEbd@cluster0.yuebknq.mongodb.net?ssl=true'
 }
--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const cors = require('cors')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 
 require('./models')
 const { config } = require('./config')
-const {PORT} = config
+const { PORT } = config
 const product = require('./routes/product')
 const customer = require('./routes/customer')
 const order = require('./routes/order')
 const category = require('./routes/category')
 
-app.use('/product',product)
-app.use('/customer',customer)
-app.use('/order',order)
-app.use('/category',category,(e)=>{
+app.use('/product', product)
+app.use('/customer', customer)
+app.use('/order', order)
+app.use('/category', category, (e) => {
   console.log(e)
 })
 
